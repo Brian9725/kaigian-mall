0de630dd3fa3461226fd8815382f401915181103
BrianHu <1786104060@qq.com>
2021-11-16 09:54:54 +0800

    将所有换行符替换为Linux换行符

--- kaigian-admin/src/main/java/pers/brian/mall/modules/pms/controller/PmsProductController.java
+++ kaigian-admin/src/main/java/pers/brian/mall/modules/pms/controller/PmsProductController.java
@@ -1,9 +1,17 @@
 package pers.brian.mall.modules.pms.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pers.brian.mall.common.api.CommonResult;
+import pers.brian.mall.modules.pms.model.PmsProduct;
+import pers.brian.mall.modules.pms.service.PmsProductService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pms/pmsProduct")
 public class PmsProductController {
 
+	private final PmsProductService productService;
+
+	@Autowired
+	public PmsProductController(PmsProductService productService) {
+		this.productService = productService;
+	}
+
+	@ApiOperation("查询商品")
+	@RequestMapping(value = "list", method = RequestMethod.POST)
+	@ResponseBody
+	public CommonResult<List<PmsProduct>> list() {
+		List<PmsProduct> products = productService.list();
+		return CommonResult.success(products);
+	}
 }
 
--- kaigian-admin/src/main/java/pers/brian/mall/modules/ums/controller/UmsMemberLevelController.java
+++ kaigian-admin/src/main/java/pers/brian/mall/modules/ums/controller/UmsMemberLevelController.java
@@ -1,21 +1,21 @@
 package pers.brian.mall.modules.ums.controller;
 
 
-import pers.brian.mall.common.api.CommonResult;
-import pers.brian.mall.modules.ums.model.UmsMemberLevel;
-import pers.brian.mall.modules.ums.service.UmsMemberLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pers.brian.mall.common.api.CommonResult;
+import pers.brian.mall.modules.ums.model.UmsMemberLevel;
+import pers.brian.mall.modules.ums.service.UmsMemberLevelService;
 
 import java.util.List;
 
 /**
- * @Description:  * <p>
- *  * 会员等级表 前端控制器
- *  * </p>
+ * @Description: <p>
+ * 会员等级表 前端控制器
+ * </p>
  * @Author: BrianHu
  * @Create: 2021-11-11 11:11
  * @Version: 0.0.1
@@ -24,20 +24,23 @@ import java.util.List;
 @RequestMapping("/memberLevel")
 public class UmsMemberLevelController {
 
-    @Autowired
-	UmsMemberLevelService memberLevelService;
+	private final UmsMemberLevelService memberLevelService;
 
-    /**
-     *   url:'/memberLevel/list',
-     *     method:'get',
-     *     params:{defaultStatus: 0}
-     */
-    @RequestMapping(value="/list",method = RequestMethod.GET)
-    public CommonResult list(
-            @RequestParam(value="defaultStatus",defaultValue = "0") Integer defaultStatus)
-    {
-        List<UmsMemberLevel> list= memberLevelService.list(defaultStatus);
-        return CommonResult.success(list);
-    }
+	@Autowired
+	public UmsMemberLevelController(UmsMemberLevelService memberLevelService) {
+		this.memberLevelService = memberLevelService;
+	}
+
+	/**
+	 * url:'/memberLevel/list',
+	 * method:'get',
+	 * params:{defaultStatus: 0}
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public CommonResult list(
+			@RequestParam(value = "defaultStatus", defaultValue = "0") Integer defaultStatus) {
+		List<UmsMemberLevel> list = memberLevelService.list(defaultStatus);
+		return CommonResult.success(list);
+	}
 }
 
