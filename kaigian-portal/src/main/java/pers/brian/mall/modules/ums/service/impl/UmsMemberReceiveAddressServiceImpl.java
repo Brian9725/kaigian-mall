1c22d8934a49cdd289665b880e9e3f52ea156ef4
BrianHu <1786104060@qq.com>
2021-12-07 11:27:22 +0800

    添加收货地址的后端接口

--- kaigian-portal/src/main/java/pers/brian/mall/modules/ums/service/impl/UmsMemberReceiveAddressServiceImpl.java
+++ kaigian-portal/src/main/java/pers/brian/mall/modules/ums/service/impl/UmsMemberReceiveAddressServiceImpl.java
@@ -1,10 +1,15 @@
 package pers.brian.mall.modules.ums.service.impl;
 
-import pers.brian.mall.modules.ums.model.UmsMemberReceiveAddress;
-import pers.brian.mall.modules.ums.mapper.UmsMemberReceiveAddressMapper;
-import pers.brian.mall.modules.ums.service.UmsMemberReceiveAddressService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pers.brian.mall.modules.ums.mapper.UmsMemberReceiveAddressMapper;
+import pers.brian.mall.modules.ums.model.UmsMemberReceiveAddress;
+import pers.brian.mall.modules.ums.service.UmsMemberReceiveAddressService;
+import pers.brian.mall.modules.ums.service.UmsMemberService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +22,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class UmsMemberReceiveAddressServiceImpl extends ServiceImpl<UmsMemberReceiveAddressMapper, UmsMemberReceiveAddress> implements UmsMemberReceiveAddressService {
 
+    @Autowired
+    private UmsMemberService memberService;
+
+    @Override
+    public Boolean add(UmsMemberReceiveAddress address) {
+        Long currentMemberId = memberService.getCurrentMember().getId();
+        address.setMemberId(currentMemberId);
+        return this.save(address);
+    }
+
+    @Override
+    public Boolean edit(UmsMemberReceiveAddress address) {
+        return this.updateById(address);
+    }
+
+    @Override
+    public Boolean delete(Long ids) {
+        return this.removeById(ids);
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> listByMemberId() {
+        QueryWrapper<UmsMemberReceiveAddress> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(UmsMemberReceiveAddress::getMemberId, memberService.getCurrentMember().getId());
+        return this.list(queryWrapper);
+    }
 }
